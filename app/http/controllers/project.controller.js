46fd04305b115c2d8c8917be129c283a3a8575fe
erfanyousefi <coffeelearngroup@gmail.com>
2022-03-15 11:52:16 +0330

    add list of project method

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -3,9 +3,10 @@ const { ProjectModel } = require("../../models/project");
 class ProjectController {
   async createProject(req, res, next) {
     try {
-      const {title, text, image } = req.body;
+      const {title, text, image, tags } = req.body;
+      console.log(tags);
       const owner = req.user._id
-      const result = await ProjectModel.create({title, text, owner, image})
+      const result = await ProjectModel.create({title, text, owner, image, tags})
       if(!result) throw {status : 400, message : "افزودن پروژه با مشکل مواجه شد"}
       return res.status(201).json({
         status : 201,
@@ -16,7 +17,19 @@ class ProjectController {
       next(error)
     }
   }
-  getAllProject() {}
+  getAllProject(req, res, next) {
+    try {
+      const owner = req.user._id;
+      const projects = await ProjectModel.find({owner});
+      return res.status(200).json({
+        status : 200,
+        success : true,
+        projects
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
   getProjectById() {}
   getAllProjectOfTeam() {}
   getProjectOfUser() {}
